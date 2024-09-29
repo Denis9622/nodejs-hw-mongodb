b8fc181acb16a0a53c5e916fcd3fab90fa5fa594
Denis9622 <152106321+Denis9622@users.noreply.github.com>
2024-09-29 16:30:04 +0300

    24324rwetwrtter

--- src/env.js
+++ src/env.js
@@ -1,29 +1,20 @@
-
-import fs from 'fs';
+import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+// Получаем текущий путь (__dirname не доступен в ES Modules)
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Шлях до .env файлу
-const envFilePath = path.resolve(__dirname, './../.env');
+// Загружаем переменные окружения из .env файла
+dotenv.config({ path: path.resolve(__dirname, './../.env') });
+
+// Теперь переменные окружения автоматически загружены в process.env
+// Вы можете получить доступ к переменным через process.env, например:
+// const dbUser = process.env.MONGODB_USER;
+// const dbPassword = process.env.MONGODB_PASSWORD;
 
 export function parseEnv() {
-  if (!fs.existsSync(envFilePath)) {
-    throw new Error('.env file does not exist');
-  }
-
-  const envContent = fs.readFileSync(envFilePath, 'utf-8');
-
-  const envVars = {};
-
-  envContent.split('\n').forEach((line) => {
-    const [key, value] = line.split('=');
-    if (key && value) {
-      envVars[key.trim()] = value.trim();
-    }
-  });
-
-  return envVars;
+  // Если нужно, вы можете обрабатывать process.env далее или просто возвращать его
+  return process.env;
 }
