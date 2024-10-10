bc6d4223271197b353b71d8da6eb4403cd09b3c8
Denis9622 <152106321+Denis9622@users.noreply.github.com>
2024-10-11 00:48:14 +0300

    wrewtryytr

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,34 +1,34 @@
-// import nodemailer from 'nodemailer';
-// import dotenv from 'dotenv';
-// import path from 'path';
-// import { SMTP } from '../constants/contacts-constants.js';
-// import { env } from './../env.js';
-// import { fileURLToPath } from 'url';
-// // Получаем текущий путь (__dirname не доступен в ES Modules)
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+import path from 'path';
+import { SMTP } from '../constants/contacts-constants.js';
+import { env } from './../env.js';
+import { fileURLToPath } from 'url';
+// Получаем текущий путь (__dirname не доступен в ES Modules)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// // Загружаем переменные окружения из .env файла
-// dotenv.config({ path: path.resolve(__dirname, './../.env') });
+// Загружаем переменные окружения из .env файла
+dotenv.config({ path: path.resolve(__dirname, './../.env') });
 
-// // Настройка транспортера для отправки писем через nodemailer
-// const transporter = nodemailer.createTransport({
-//   host: env(SMTP.SMTP_HOST), // Получаем хост из переменных окружения через функцию env()
-//   port: Number(env(SMTP.SMTP_PORT)), // Получаем порт из переменных окружения
-//   auth: {
-//     user: env(SMTP.SMTP_USER), // Пользователь SMTP
-//     pass: env(SMTP.SMTP_PASSWORD), // Пароль SMTP
-//   },
-// });
+// Настройка транспортера для отправки писем через nodemailer
+const transporter = nodemailer.createTransport({
+  host: env(SMTP.SMTP_HOST), // Получаем хост из переменных окружения через функцию env()
+  port: Number(env(SMTP.SMTP_PORT)), // Получаем порт из переменных окружения
+  auth: {
+    user: env(SMTP.SMTP_USER), // Пользователь SMTP
+    pass: env(SMTP.SMTP_PASSWORD), // Пароль SMTP
+  },
+});
 
-// // Функция для отправки письма
-// export const sendEMail = async (mailOptions) => {
-//   try {
-//     const result = await transporter.sendMail(mailOptions);
-//     return result;
-//   } catch (error) {
-//     console.error('Failed to send the email, please try again later:', error);
-//     throw new Error('Failed to send the email, please try again later');
-//   }
-// };
-// //Failed to send the email, please try again later
+// Функция для отправки письма
+export const sendEMail = async (mailOptions) => {
+  try {
+    const result = await transporter.sendMail(mailOptions);
+    return result;
+  } catch (error) {
+    console.error('Failed to send the email, please try again later:', error);
+    throw new Error('Failed to send the email, please try again later');
+  }
+};
+//Failed to send the email, please try again later
