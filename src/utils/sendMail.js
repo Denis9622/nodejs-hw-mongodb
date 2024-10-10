aea39229017e3da9a932430d1e46385aa3503e69
Denis9622 <152106321+Denis9622@users.noreply.github.com>
2024-10-11 00:44:23 +0300

    fix

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,6 +1,15 @@
 import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+import path from 'path';
 import { SMTP } from '../constants/contacts-constants.js';
 import { env } from './../env.js';
+import { fileURLToPath } from 'url';
+// Получаем текущий путь (__dirname не доступен в ES Modules)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Загружаем переменные окружения из .env файла
+dotenv.config({ path: path.resolve(__dirname, './../.env') });
 
 // Настройка транспортера для отправки писем через nodemailer
 const transporter = nodemailer.createTransport({
@@ -17,7 +26,9 @@ export const sendEMail = async (mailOptions) => {
   try {
     const result = await transporter.sendMail(mailOptions);
     return result;
-  } catch {
-    throw new Error('Failed to send the email, please try again later.');
+  } catch (error) {
+    console.error('Failed to send the email, please try again later:', error);
+    throw new Error('Failed to send the email, please try again later');
   }
 };
+//Failed to send the email, please try again later
